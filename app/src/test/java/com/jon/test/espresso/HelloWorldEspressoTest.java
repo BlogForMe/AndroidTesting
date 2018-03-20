2b90d6c1613e74f91e3ba2ee760a2386c86625be
Jon <huaiyiheyuan@gmail.com>
2018-03-20 08:06:32 +0800

    测试

---
+++ app/src/test/java/com/jon/test/espresso/HelloWorldEspressoTest.java
@@ -0,0 +1,29 @@
+package com.jon.test.espresso;
+
+import android.test.suitebuilder.annotation.LargeTest;
+
+import com.jon.test.MainActivity;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static java.util.regex.Pattern.matches;
+
+/**
+ * Created by jon on 3/20/18.
+ */
+
+//@RunWith(AndroidJUnit4.class)
+//@LargeTest
+//public class HelloWorldEspressoTest {
+//
+//    @Rule
+//    public ActivityTestRule<MainActivity> mActivityRule =
+//            new ActivityTestRule(MainActivity.class);
+//
+//    @Test
+//    public void listGoesOverTheFold() {
+//        onView(withText("Hello world!")).check(matches(isDisplayed()));
+//    }
+//}
